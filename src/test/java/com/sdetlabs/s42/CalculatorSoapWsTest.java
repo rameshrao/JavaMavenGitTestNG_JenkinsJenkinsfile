a8100bbe55e4b59b5bcfdf8c25940fe77e4c9709
rameshrao <rameshrao.2006@gmail.com>
2023-10-09 17:52:08 +0530

    Formatting changes 9 Oct 2023 1752

--- src/test/java/com/sdetlabs/s42/CalculatorSoapWsTest.java
+++ src/test/java/com/sdetlabs/s42/CalculatorSoapWsTest.java
@@ -12,7 +12,7 @@ public class CalculatorSoapWsTest {
 	String expResponse = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><AddResponse xmlns=\"http://tempuri.org/\"><AddResult>46</AddResult></AddResponse></soap:Body></soap:Envelope>";
 
 	@Test
-  public void getCalculatorAddOperationTest() throws MalformedURLException, IOException {
-	  assertEquals(CalculatorSoapWs.getCalculatorAddOperation(23, 23), expResponse);
-  }
+	public void getCalculatorAddOperationTest() throws MalformedURLException, IOException {
+		assertEquals(CalculatorSoapWs.getCalculatorAddOperation(23, 23), expResponse);
+	}
 }
